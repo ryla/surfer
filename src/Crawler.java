c1fb5b07f437c7ffedbdfec6eb11dba80cfcb207
brendan <brendan.ritter@yahoo.com>
2013-03-27 23:27:45 -0400

    Refactor and functional Crawler

--- src/Crawler.java
+++ src/Crawler.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
@@ -11,12 +12,67 @@ import org.jsoup.select.Elements;
  *
  */
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Transaction;
+
 /**
  * @author neghtebas and brendan ritter
  *
  */
 public class Crawler {
 
+	private Jedis jedis;
+	
+	private void addLinks(HashSet<String> links){
+		for(String link: links){
+			addLink(link);
+		}
+	}
+	
+	public boolean seed(String url) {
+		addLink(url);
+		return jedis.scard("toCawl")>0;
+	}
+	
+	private void addLink(String link){
+		if (!jedis.sismember("triedCrawl",link)){
+			jedis.sadd("toCrawl", link);
+		}
+	}
+	
+	public Crawler(String hostname){
+		this.jedis=new Jedis(hostname);
+	}
+	
+	public void run() {
+		while (true) {
+			runOnce();
+		}
+	}
+	
+	public void run(int count) {
+		for (int i = 0; i < count; i++) {
+			runOnce();
+		}
+	}
+	
+	public boolean runOnce(){
+		Document doc=null;
+		String url = jedis.spop("toCrawl");
+		jedis.sadd("triedCrawl", url);
+		
+		try {
+			Connection con = Jsoup.connect(url);
+			doc=con.get();
+		} catch (IOException e) {
+			return false;
+		} 
+
+		addLinks(getLinks(doc));
+		
+		return true;
+	}
     public Document htmlOut(String url){
    	 Document doc=null;
    	 try {
@@ -31,7 +87,7 @@ public class Crawler {
     }
     
     //Identifies all the absurl links in a given html doc.
-    public HashSet<String> linkIdentifier(Document doc){
+    private static HashSet<String> getLinks(Document doc){
    	 HashSet<String> uniqueLinks=new HashSet<String>();
    	 Elements links = doc.getElementsByTag("a");
    	 for (Element link : links) {
@@ -41,17 +97,14 @@ public class Crawler {
    	 }
    	 return uniqueLinks;
     }
-    public String textIdentifier(Document doc){
+    
+    private static String getText(Document doc){
    	 //Element head=doc.head();
    	 //String allText=head.text();
    	 String allText= doc.text();
    	 System.out.println(doc.text());
    	 return allText;
    	 
-    }
-    public void emit(String curUrl,HashSet<String> allLinks,String allText){    
-   	 HashMap<String, String[]> indexin= new HashMap<String, String[]>();
-   	 //Somehow sent to indexer
     }
     
 }
--- src/Surfer.java
+++ src/Surfer.java
@@ -1,41 +1,9 @@
-import java.io.IOException;
-import java.util.HashSet;
-
-import org.jsoup.*;
-import org.jsoup.nodes.*;
-import org.jsoup.select.*;
-
-import redis.clients.jedis.Jedis;
-
 
 public class Surfer {
 
 	public static void main(String[] args) {
-		Jedis jedis = new Jedis("localhost");	
-		jedis.sadd("toCrawl", "http://www.olin.edu");
-		for (int i = 0; i < 500; i++) {
-			Document doc;
-			String url = jedis.spop("toCrawl");
-			try {
-				doc = Jsoup.connect(url).get();
-				Elements elems = doc.getElementsByAttribute("href");
-				HashSet<String> links = new HashSet<String>();
-				for (Element elem: elems) {
-					links.add(elem.attr("abs:href"));
-				}
-				for (String link: links) {
-					if (! jedis.sismember("alreadyCrawled", link) && 
-							! jedis.sismember("failedCrawl", url)) {
-						jedis.sadd("toCrawl", link);
-					}
-				}
-				jedis.sadd("alreadyCrawled", url);
-				System.out.println(url);
-			} catch (IOException e) {
-				jedis.sadd("failedCrawl", url);
-			} 
-		}
+		Crawler crawler = new Crawler("localhost");
+		crawler.seed("http://www.olin.edu/");
+		crawler.run(500);
 	}
-
-
 }
