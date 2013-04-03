855e70281d20f5bcfce40fd67021b2a129e457ae
NCEghtebas <nassim.eghtebas@sudents.olin.edu>
2013-04-03 14:28:37 -0400

    Added Searcher.java

---
+++ src/Searcher.java
@@ -0,0 +1,19 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author Chloe Eghtebas
+ * @author Reily Buttler
+ *
+ */ 
+public class Searcher {
+
+	public Searcher(Jedis jedis){
+		this.jedis = jedis;
+	}
+	
+}
