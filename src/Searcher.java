c2ed24f703fcf9fca28c2df417c2f5efb30d5c26
NCEghtebas <nassim.eghtebas@sudents.olin.edu>
2013-04-03 15:40:14 -0400

    Added Comments

--- src/Searcher.java
+++ src/Searcher.java
@@ -20,6 +20,14 @@ public class Searcher {
 		this.jedis = jedis;
 	}
 	
+	/**
+	 * This method takes a search keyword and integer n and returns an 
+	 * ArrayList of the most relevant URL's of length n.
+	 * 
+	 * @param keyword
+	 * @param n
+	 * @return n most relevant search results
+	 */
 	public ArrayList<URL> lookup(String keyword, int n){
 		ArrayList<URL> urls = new ArrayList<URL>();
 		for (String index: jedis.zrevrange(keyword, 0, n -1)) {
@@ -32,6 +40,10 @@ public class Searcher {
 		return urls;	
 	} 
 	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Searcher s= new Searcher(new Jedis("localhost"));
 		ArrayList<URL> urlist= s.lookup("olin" ,5);
