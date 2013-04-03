3a3419bb4c50c82ccbda4b9938b26c807a942ec1
NCEghtebas <nassim.eghtebas@sudents.olin.edu>
2013-04-03 15:00:48 -0400

    Added lookup function, returns a length n list of the most relevant urls

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,3 +1,7 @@
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
 import redis.clients.jedis.*;
 
 /**
@@ -16,6 +20,22 @@ public class Searcher {
 		this.jedis = jedis;
 	}
 	
+	public ArrayList<URL> lookup(String keyword, int n){
+		ArrayList<URL> urls = new ArrayList<URL>();
+		for (String index: jedis.zrevrange(keyword, 0, n -1)) {
+			String url = jedis.hget("urlIndex", index);
+			try {
+				urls.add(new URL(url));
+			} catch (MalformedURLException e) { }
+		}
+		
+		return urls;	
+	} 
 	
+	public static void main(String[] args) {
+		Searcher s= new Searcher(new Jedis("localhost"));
+		ArrayList<URL> urlist= s.lookup("olin" ,5);
+		System.out.println(urlist.toString());
+	}
 	
 }
