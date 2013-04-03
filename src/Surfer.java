5cbfb86ada7952e95dd8895e1ad2d6a24bb77054
patimartinvicente <patimartinvicente@gmail.com>
2013-04-03 14:29:09 -0400

    ReImplemented the zincrby and test the quality ranking

--- src/Surfer.java
+++ src/Surfer.java
@@ -4,13 +4,13 @@ public class Surfer {
 
 	public static void main(String[] args) {
 		Crawler crawlerSeed = new Crawler(new Jedis("localhost"));
-		crawlerSeed.seed("http://www.olin.edu/");
+		crawlerSeed.seed("http://loquimity.org/a.html");
 		Parser parser = new Parser(new Jedis("localhost"));
-		parser.verbose();
+		//parser.verbose();
 		Thread[] crawlerPool = new Thread[10];
 		for(Thread thread: crawlerPool){
 			Crawler crawler = new Crawler(new Jedis("localhost"));
-			crawler.verbose();
+			//crawler.verbose();
 			thread = new Thread(crawler);
 			thread.start();
 		}
