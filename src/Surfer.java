ce4a44ee363eca11624a64f35d83f4b33fd1bfe4
patimartinvicente <patimartinvicente@gmail.com>
2013-04-03 15:37:12 -0400

    we can get the highest element from the ranked sorted set

--- src/Surfer.java
+++ src/Surfer.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.SortedSet;
+
 import redis.clients.jedis.*;
 
 public class Surfer {
+	
+	public static void getHighest()
+	{
+		
+		Jedis jedis = new Jedis("localhost");
+		Set<String> sortedSet = jedis.zrevrange("urlScore", 0, 0);
+		Object [] arraySet = sortedSet.toArray();
+		int highest = jedis.zscore("urlScore", (String)arraySet[0]).intValue();
+		System.out.println(highest);
+	}
 
 	public static void main(String[] args) {
 		Crawler crawlerSeed = new Crawler(new Jedis("localhost"));
-		crawlerSeed.seed("http://loquimity.org/a.html");
+		crawlerSeed.seed("http://loquimity.org/c.html");
 		Parser parser = new Parser(new Jedis("localhost"));
 		//parser.verbose();
 		Thread[] crawlerPool = new Thread[10];
@@ -16,5 +31,8 @@ public class Surfer {
 		}
 		Thread parserThread= new Thread(parser);
 		parserThread.start();
+		getHighest();
 	}
+	
+
 }
