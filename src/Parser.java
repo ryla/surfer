d8cd91d024253e96351bc04da8be7f67f6e713f9
patimartinvicente <patimartinvicente@gmail.com>
2013-04-05 17:45:27 -0400

    authors added

--- src/Parser.java
+++ src/Parser.java
@@ -4,8 +4,10 @@ import java.util.List;
 import redis.clients.jedis.*;
 
 /**
+ * @author Chloe Eghtebas
  * @author Brendan Ritter
  * @author Riley Butler
+ * @author Pati Martin
  */
 public class Parser implements Runnable {
 
