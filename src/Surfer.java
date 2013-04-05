ece18e4146b77ecc492829ea1ec67b1acf1a56cc
Riley Alethea Butler <riley.butler@students.olin.edu>
2013-04-05 16:00:36 -0400

    Cleaned up Surfer.main

--- src/Surfer.java
+++ src/Surfer.java
@@ -1,21 +1,13 @@
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.SortedSet;
-
 import redis.clients.jedis.*;
 
 public class Surfer {
 	public static void main(String[] args) {
 		Crawler crawlerSeed = new Crawler(new Jedis("localhost"));
-		//http://loquimity.org/c.html
 		crawlerSeed.seed("http://www.olin.edu");
 		Parser parser = new Parser(new Jedis("localhost"));
-		//parser.verbose();
 		Thread[] crawlerPool = new Thread[10];
 		for(Thread thread: crawlerPool){
 			Crawler crawler = new Crawler(new Jedis("localhost"));
-			//crawler.verbose();
 			thread = new Thread(crawler);
 			thread.start();
 		}
