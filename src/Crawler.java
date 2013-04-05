377d1761d4082534ee6d2b6b09253788925834c8
brendan <brendan.ritter@yahoo.com>
2013-04-05 17:41:20 -0400

    Commented Crawler. HUZZAH!

--- src/Crawler.java
+++ src/Crawler.java
@@ -13,6 +13,7 @@ import redis.clients.jedis.Jedis;
  * @author Chloe Eghtebas
  * @author Brendan Ritter
  * @author Riley Butler
+ * @author Pati Martin
  */
 public class Crawler implements Runnable{
 
@@ -23,19 +24,30 @@ public class Crawler implements Runnable{
 	public Crawler(Jedis jedis){
 		this.jedis = jedis;
 	}
-
+	/**
+	 * Runs continuously until you run out of memory or you interrupt it.
+	 */
 	public void run() {
 		while (true) {
 			runOnce();
 		}
 	}
-	
+	/**
+	 * Gets attempts number of urls from toCrawl and finds all their urls.
+	 * 
+	 * @param how many times to run
+	 */
 	public void run(int attempts) {
 		for (int i = 0; i < attempts; i++) {
 			runOnce();
 		}
 	}
-	
+	/**
+	 * Gets one url from jedis key 'toCrawl' connects to it 
+	 * and adds all found urls to jedis key 'toParse'. 
+	 * 
+	 * @return Whether the function ran correctly or not
+	 */
 	public boolean runOnce(){
 		Document doc = null;
 		Connection con = null;
@@ -67,7 +79,12 @@ public class Crawler implements Runnable{
 		jedis.rpush("toParse", index.toString() + ":" + getText(doc));
 		return true;
 	}
-
+/**
+ * A wrapper for addLink. This function starts crawling at the given url.
+ * 
+ * @param url, the url to begin searching at.
+ * @return whether or not the function worked.
+ */
 	public boolean seed(String url) {
 		addLink(url);
 		return jedis.scard("toCawl")>0;
@@ -89,27 +106,45 @@ public class Crawler implements Runnable{
 	public void silent() {
 		verb = false;
 	}
-	
+	/**
+	 * Adds an index to jedis key 'urlIndex' This operates as a lookup from url to index. 
+	 * @param url
+	 * @return
+	 */
 	private long addIndex(String url) {
 		Long index = jedis.incr("urlCount");
 		jedis.hset("urlIndex", index.toString(), url);
 		return (long)index;
 	}
 	
+	/**
+	 * Records a url to be crawled if it hasn't already. 
+	 * Also keeps track of how many links go to the url for quality scoring purposes.
+	 * 
+	 * @param link
+	 */
 	private void addLink(String link){
 		if (!jedis.sismember("triedCrawl",link)){
 			jedis.sadd("toCrawl", link);
 		}
-		System.out.println(link);
 		jedis.zincrby("urlScore", 1, link);
 	}
-	
+	/**
+	 * Adds a multiple of links to be searched. 
+	 * A wrapper for addLink.
+	 * @param links
+	 */
 	private void addLinks(HashSet<String> links){
 		for(String link: links){
 			addLink(link);
 		}
 	}
-	
+	/**
+	 * Returns all links found on a webpage. 
+	 * 
+	 * @param doc
+	 * @return Returns all unique links on a page.
+	 */
 	private static HashSet<String> getLinks(Document doc){
 		HashSet<String> uniqueLinks=new HashSet<String>();
 		Elements links = doc.getElementsByTag("a");
@@ -119,7 +154,12 @@ public class Crawler implements Runnable{
 		}
 		return uniqueLinks;
 	}
-
+	/**
+	 * Gets all text on a webpage
+	 * 
+	 * @param doc
+	 * @return all the text on a webpage
+	 */
 	private static String getText(Document doc){
 		String allText = doc.title() + " " +doc.text();
 		return allText;
